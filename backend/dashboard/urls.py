d2141353b1214c779eb2d94ca29232edfee3384f
Nirika Lamichhane <nirikalamichhane12@gmail.com>
2025-07-15 22:26:06 +0545

    dashboard on progress

--- backend/dashboard/urls.py
+++ backend/dashboard/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PlaceViewSet, HotelViewSet, FoodViewSet, ActivityViewSet
+from .views import PlaceViewSet, HotelViewSet, FoodViewSet, ActivityViewSet, FavoriteViewSet, CommentViewSet        
 
 router = DefaultRouter()
 router.register(r'places', PlaceViewSet)
 router.register(r'hotels', HotelViewSet)
 router.register(r'foods', FoodViewSet)
 router.register(r'activities', ActivityViewSet)
+router.register(r'favorites', FavoriteViewSet)
+router.register(r'comments', CommentViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),
--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
-from .models import Place, Hotel, Food, Activity
-from .serializers import PlaceSerializer, HotelSerializer, FoodSerializer, ActivitySerializer
+from rest_framework.permissions import IsAuthenticated
+from .models import Place, Hotel, Food, Activity, Favorite, Comment
+from .serializers import PlaceSerializer, HotelSerializer, FoodSerializer, ActivitySerializer, FavoriteSerializer, CommentSerializer
 
 class PlaceViewSet(viewsets.ModelViewSet):
     queryset = Place.objects.all()
@@ -20,3 +21,21 @@ class FoodViewSet(viewsets.ModelViewSet):
 class ActivityViewSet(viewsets.ModelViewSet):
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
+
+class FavoriteViewSet(viewsets.ModelViewSet):
+    queryset = Favorite.objects.all()
+    serializer_class = FavoriteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create (self, serializer):
+        serializer.save(user=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    

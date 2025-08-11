35fc2eab4ad9d9a080febec4b8f1e57f39305fc6
Nirika Lamichhane <nirikalamichhane12@gmail.com>
2025-08-11 07:51:00 +0545

    chenged

--- ml_recommender/content_based.py
+++ ml_recommender/content_based.py
@@ -1,2 +1,3 @@
 # this is the content based here the datas will be related with the help of the users pregerenrenc
 print("content based")
+print("trying to keep the github streak but forget yesterday huhu")

d1543aaa90e31f7848d5e95dd43fa644bf265c16
Nirika Lamichhane <nirikalamichhane12@gmail.com>
2025-08-13 22:26:27 +0545

    chenged

--- ml_recommender/content_based.py
+++ ml_recommender/content_based.py
@@ -2,3 +2,4 @@
 print("content based")
 print("trying to keep the github streak but forget yesterday huhu")
 print("haha its august 12 of the 2025 an i am trying my best to keep streak")
+print("its august 13 and i had the worst day, didnt write anything by myself on exam, copied from amrit and then in the tuition the child cried and comming hostel i did nothing gave test of research paper 4/6 then i am in dilemma if i should text se sir or not")

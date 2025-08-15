15161fb2fd7bd792a3b423ce2e675c52b083c184
Nirika Lamichhane <nirikalamichhane12@gmail.com>
2025-08-15 10:57:06 +0545

    daily streak

--- ml_recommender/content_based.py
+++ ml_recommender/content_based.py
@@ -5,3 +5,5 @@ print("haha its august 12 of the 2025 an i am trying my best to keep streak")
 print("its august 13 and i had the worst day, didnt write anything by myself on exam, copied from amrit and then in the tuition the child cried and comming hostel i did nothing gave test of research paper 4/6 then i am in dilemma if i should text se sir or not")
 
 print("its august 14 and today i spent the day sleeping in bed")
+
+print(" its august 15 and today i read the instrumentation chapter 8 and then thoroughly went through chapter 9. had lunch and made the lab report of use case diagram of the software engineering and currently doing this")

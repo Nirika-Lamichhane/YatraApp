4c01ab2a4da89182bf9823bb208e54f2dec4bdde
Nirika Lamichhane <nirikalamichhane12@gmail.com>
2025-10-19 12:32:45 +0545

    changes

--- backend/dashboard/chatbot/views.py
+++ backend/dashboard/chatbot/views.py
@@ -20,3 +20,4 @@ def chatbot_api(request):
 
 print( "  back with my aspire module and posted the feedback on my professional objectives. ")
 print(" i am currently watching the pre master class materials for the master class which is uspposed to be at oct 23rd on the day of tihar bhaitika")
+print ( " i did the pre class reading of one master class and another also but 1 topic.")

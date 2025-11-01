921e190e778124436d644cf6735bea84ed884a22
Nirika Lamichhane <nirikalamichhane12@gmail.com>
2025-11-01 22:42:55 +0545

    added

--- backend/dashboard/chatbot/formatter.py
+++ backend/dashboard/chatbot/formatter.py
@@ -87,4 +87,6 @@ def format_destination_types(types):
 
 
 print(" this is me after exm and even though my ecams are good enough i am manifesting to top this exam as well with any marks.")
-print(" i am memorizing and manidesting myself as the one toping this semester as well ")
+print(" i am memorizing and manifesting myself as the one toping this semester as well ")
+
+print (" i am watching the remote job masterclas in youtube as i couldnot attend it on zoom.")

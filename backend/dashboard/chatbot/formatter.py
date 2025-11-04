a492f4afde7cdb1871fe61c2e29e319b8854ecc9
Nirika Lamichhane <nirikalamichhane12@gmail.com>
2025-11-04 07:26:20 +0545

    added

--- backend/dashboard/chatbot/formatter.py
+++ backend/dashboard/chatbot/formatter.py
@@ -87,3 +87,4 @@ def format_destination_types(types):
 
 print(" its the second day of preparation for exam and now i am going to prepare for that from today")
 print(" i read till 1:41 and then came to my room and its 4:19 now watched young sheldon till now")
+print(" bought a pant worth 750 and its good enough to wear to college. ")

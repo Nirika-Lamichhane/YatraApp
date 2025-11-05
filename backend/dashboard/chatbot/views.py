47b627ed4572981993abf8fbb4e967ce787b7539
Nirika Lamichhane <nirikalamichhane12@gmail.com>
2025-11-05 06:36:33 +0545

    added

--- backend/dashboard/chatbot/views.py
+++ backend/dashboard/chatbot/views.py
@@ -19,3 +19,4 @@ def chatbot_api(request):
 
 print(" its 1:43 now and i am going to research about IEEEXtreme environment and all. ")
 print(" its the november 4 last day for exam prep of english and its 3 09 now")
+print(" its nov 5 and 6:36 and now i am going to revise for my english exam")

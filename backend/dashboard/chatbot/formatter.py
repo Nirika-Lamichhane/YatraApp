1c8938a0498a6a4d47a1f15bc36377ba3200c295
Nirika Lamichhane <nirikalamichhane12@gmail.com>
2025-11-11 13:17:28 +0545

    added

--- backend/dashboard/chatbot/formatter.py
+++ backend/dashboard/chatbot/formatter.py
@@ -85,7 +85,8 @@ def format_destination_types(types):
         message += f"{i}. {t.name}\n"
     return message
 
-
+'''
 print(" hey its second day and till now i have completed chp 5 and 3 and its 9:06 am right now and its the second day of 5 days intensice ai agent course. ")
 print(" yesterday was only 2 commits i guess ")
 print(" its 1:16 pm and i completed chapter 8 and had chatpat till now")
+'''
